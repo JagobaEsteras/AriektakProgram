53368f139c2fd7d9a5cc25a9da580a916cd872d4
JagobaEsteras <JagobaEsteras@DESKTOP-RQNJPTT>
2020-03-09 09:25:33 +0100

    Ariketak etxean egiteko

---
+++ src/modelo/dao/LibroModelo.java
@@ -0,0 +1,13 @@
+package modelo.dao;
+
+import java.util.ArrayList;
+
+import modelo.Conector;
+import modelo.bean.Libro;
+
+public class LibroModelo extends Conector{
+	
+	public Libro select(int id){
+		
+	}
+}
