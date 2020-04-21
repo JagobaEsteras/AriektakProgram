4f642d8ff0f727790b81d63e736dc07a68d52e3d
Jagoba <Jagoba@WIN8.home>
2020-04-21 09:39:09 +0200

    Api

---
+++ src/controlador/InscribirUsuario.java
@@ -0,0 +1,54 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.bean.Actividad;
+import modelo.bean.Usuario;
+import modelo.dao.ModeloActividad;
+import modelo.dao.ModeloInscripcion;
+import modelo.dao.ModeloUsuario;
+
+/**
+ * Servlet implementation class InscribirUsuario
+ */
+@WebServlet("/InscribirUsuario")
+public class InscribirUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InscribirUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idActividad = Integer.parseInt(request.getParameter("idactividad"));
+		int idUsuario = Integer.parseInt(request.getParameter("idusuarios"));
+		
+		ModeloInscripcion mInscripcion = new ModeloInscripcion();
+		mInscripcion.inscribir(idUsuario, idActividad);
+		
+		response.sendRedirect("VerActividad?id="+idActividad);
+	}
+
+}
